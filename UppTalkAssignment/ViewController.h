6e04980a0c4a5ebfead2d953c0c0e070c9070cd5
Vladimir Marinov <vm@gksoft.eu>
2016-02-27 09:58:00 +0200

    Initial Commit

---
+++ UppTalkAssignment/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UppTalkAssignment
+//
+//  Created by Vladimir Marinov on 27.02.16.
+//  Copyright © 2016 Vladimir Marinov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UppTalkAssignment/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UppTalkAssignment
+//
+//  Created by Vladimir Marinov on 27.02.16.
+//  Copyright © 2016 Vladimir Marinov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
